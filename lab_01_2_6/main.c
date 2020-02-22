f82cf31592d1a7c9379d840ecc3c8557d5147e2c
Muravev Ilia <degtyarev327@gmail.com>
2020-02-22 23:05:35 +0300

    style fixes for task 6

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -2,20 +2,19 @@
 #include <math.h>
 
 float vector(float x2, float x1,
-		float y3, float y2,
-			float x3, float y1);
+float y3, float y2, float x3, float y1);
 
 int main()
 {
 	int rs;
-	float x1, x2, x3, xA, y1, y2, y3, yA;
-	rs = scanf("%f %f %f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x3, &y3, &xA, &yA);
+	float x1, x2, x3, xa, y1, y2, y3, ya;
+	rs = scanf("%f %f %f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x3, &y3, &xa, &ya);
 	if (rs == 8)
 	{
 		float a, b, c;
-		a = vector(x1, xA, y2, y1, x2, yA);
-		b = vector(x2, xA, y3, y2, x3, yA);
-		c = vector(x3, xA, y1, y3, x1, yA);
+		a = vector(x1, xa, y2, y1, x2, ya);
+		b = vector(x2, xa, y3, y2, x3, ya);
+		c = vector(x3, xa, y1, y3, x1, ya);
 		if (((a > 0) && (b > 0) && (c > 0)) || ((a < 0) && (b < 0) && (c < 0)))
 		{
 			printf("%d", 0);
@@ -36,9 +35,7 @@ int main()
 }
 
 float vector(float x2, float x1,
-		float y3, float y2,
-			float x3, float y1)
-
+float y3, float y2, float x3, float y1)
 {
 	return (x2 - x1) * (y3 - y2) - (x3 - x2) * (y2 - y1);
 }
