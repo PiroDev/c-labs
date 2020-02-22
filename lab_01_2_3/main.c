fed4e4337e9308146b9e69c3feb4eb9dc73e659f
Muravev Ilia <degtyarev327@gmail.com>
2020-02-22 17:11:52 +0300

    task 3 solution was edited

--- lab_01_2_3/main.c
+++ lab_01_2_3/main.c
@@ -4,7 +4,7 @@ int main()
 {
 	float r1, r2, r3, r;
 	scanf("%f %f %f", &r1, &r2, &r3);
-	r = (r1 * r2 * r3) / (r3 * (r2 + r1) + r1 + r2);
+	r = (r1 * r2 * r3) / (r3 * (r2 + r1) + r1 * r2);
 	printf("%f", r);
 	return 0;
 }
