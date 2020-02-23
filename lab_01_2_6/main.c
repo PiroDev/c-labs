03c7f67d014b683c8c1a7975f8db0a74fe7d9c07
Muravev Ilia <degtyarev327@gmail.com>
2020-02-23 11:33:59 +0300

    check for line was added in task 6

--- lab_01_2_6/main.c
+++ lab_01_2_6/main.c
@@ -4,28 +4,49 @@
 float vector(float x2, float x1,
 float y3, float y2, float x3, float y1);
 
+float min(float a, float b, float c);
+
+float max(float a, float b, float c);
+
+int is_line(float x1, float y1,
+float x2, float y2, float x3, float y3);
+
 int main()
 {
 	int rs;
 	float x1, x2, x3, xa, y1, y2, y3, ya;
-	rs = scanf("%f %f %f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x3, &y3, &xa, &ya);
+	rs = scanf("%f%f%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &x3, &y3, &xa, &ya);
 	if (rs == 8)
 	{
-		float a, b, c;
-		a = vector(x1, xa, y2, y1, x2, ya);
-		b = vector(x2, xa, y3, y2, x3, ya);
-		c = vector(x3, xa, y1, y3, x1, ya);
-		if (((a > 0) && (b > 0) && (c > 0)) || ((a < 0) && (b < 0) && (c < 0)))
+		if (is_line(x1, y1, x2, y2, x3, y3))
 		{
-			printf("%d", 0);
-		}
-		else if ((fabsf(a) < 1e-7) || (fabsf(b) < 1e-7) || (fabsf(c) < 1e-7))
-		{
-			printf("%d", 1);
+			if ((xa <= max(x1, x2, x3)) && (xa >= min(x1, x2, x3)))
+			{
+				printf("%d", 1);
+			}
+			else
+			{
+				printf("%d", 2);
+			}
 		}
 		else
-			printf("%d", 2);
-		return 0;
+		{
+			float a, b, c;
+			a = vector(x1, xa, y2, y1, x2, ya);
+			b = vector(x2, xa, y3, y2, x3, ya);
+			c = vector(x3, xa, y1, y3, x1, ya);
+			if (((a > 0) && (b > 0) && (c > 0)) || ((a < 0) && (b < 0) && (c < 0)))
+			{
+				printf("%d", 0);
+			}
+			else if ((fabsf(a) < 1e-7) || (fabsf(b) < 1e-7) || (fabsf(c) < 1e-7))
+			{
+				printf("%d", 1);
+			}
+			else
+				printf("%d", 2);
+			return 0;
+		}
 	}
 	else
 	{
@@ -38,4 +59,54 @@ float vector(float x2, float x1,
 float y3, float y2, float x3, float y1)
 {
 	return (x2 - x1) * (y3 - y2) - (x3 - x2) * (y2 - y1);
+}
+
+float min(float a, float b, float c)
+{
+	if (a < b)
+	{
+		if (a < c)
+		{
+			return a;
+		}
+		else
+			return c;
+	}
+	else
+	{
+		if (b < c)
+		{
+			return b;
+		}
+		else
+			return c;
+	}
+}
+
+float max(float a, float b, float c)
+{
+	if (a > b)
+	{
+		if (a > c)
+		{
+			return a;
+		}
+		else
+			return c;
+	}
+	else
+	{
+		if (b > c)
+		{
+			return b;
+		}
+		else
+			return c;
+	}
+}
+
+int is_line(float x1, float y1,
+float x2, float y2, float x3, float y3)
+{
+	return (fabsf((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) < 1e-7);
 }
