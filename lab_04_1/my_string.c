c61fb878bbf386e6fc5acb81ace62ff3a30202e7
Muravev Ilia <degtyarev327@gmail.com>
2020-04-19 15:32:23 +0300

    add solution for task 1

---
+++ lab_04_1/my_string.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include "my_string.h"
+
+char* my_strrchr(const char *str, int symbol)
+{
+    char* result = NULL;
+    int index = 0;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == symbol)
+            index = i;
+        i++;
+    }
+    if (str[index] == symbol)
+        result = (char*)(str + index);
+    return result;
+}
---
+++ lab_04_1/my_string.h
@@ -0,0 +1,1 @@
+char* my_strrchr(const char *str, int symbol);
