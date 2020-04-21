f8356edbe30b5f9725f45d0d1dc4e4770fd31d3b
Muravev Ilia <degtyarev327@gmail.com>
2020-04-21 12:14:16 +0300

    fix solution for task 2

--- lab_04_0_2/main.c
+++ lab_04_0_2/main.c
@@ -6,16 +6,17 @@ void unique_words(char[STR_MAX_LEN / 2 - 1][WORD_MAX_LEN + 1], int[], int);
 
 int main()
 {
-    char words[STR_MAX_LEN / 2 - 1][WORD_MAX_LEN + 1] = { '\0' };
+    char words[STR_MAX_LEN / 2 - 1][WORD_MAX_LEN + 1] = {'\0'};
     int words_count = 0;
     status_code result = str_input(words, &words_count);
     if (result == ok)
     {
-        int repeats[STR_MAX_LEN / 2 - 1] = { 0 };
+        int repeats[STR_MAX_LEN / 2 - 1] = {0};
         unique_words(words, repeats, words_count);
         printf("Result:\n");
         for (int i = 0; i < words_count; i++)
-            printf("%s %d\n", words[i], repeats[i]);
+            if (repeats[i] != 0)
+                printf("%s %d\n", words[i], repeats[i]);
         return result;
     }
     printf("Error!");
@@ -30,9 +31,9 @@ void unique_words(char words[STR_MAX_LEN / 2 - 1][WORD_MAX_LEN + 1], int repeats
         for (int j = i + 1; j < words_count; j++)
         {
             if ((repeats[i] != 0) && (strcmp((const char *) words[i], (const char *) words[j]) == 0))
-            {
-                repeats[i]++;
-                repeats[j]++;
-            }
+                {
+                    repeats[i]++;
+                    repeats[j] = 0;
+                }
         }
 }
