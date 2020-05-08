39c48aea4cd8e480aecc38e6071e0d9541d0af8c
Muravev Ilia <degtyarev327@gmail.com>
2020-05-08 23:41:14 +0300

    fix solution for task 1

--- lab_51_7_1/functions.c
+++ lab_51_7_1/functions.c
@@ -8,7 +8,8 @@ status_code process(FILE *f, int *count)
     int a1 = 0;
     int a2 = 0;
     int a3 = 0;
-    while (fscanf(f, "%d", &a1) == 1)
+    int scan_res = 1;
+    while ((scan_res = fscanf(f, "%d", &a1)) == 1)
     {
         n++;
         if (n >= 3)
@@ -19,7 +20,7 @@ status_code process(FILE *f, int *count)
         a3 = a2;
         a2 = a1;
     }
-    if (n == 0)
+    if ((n == 0) || (scan_res != EOF))
         result = input_error;
     return result;
 }
