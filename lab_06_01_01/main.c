88dec6c9369adc5ca9ef5bbc8ff96f84f9ad04db
Muravev Ilia <degtyarev327@gmail.com>
2020-09-09 09:48:42 +0300

    fix solution for lab_06

--- lab_06_01_01/main.c
+++ lab_06_01_01/main.c
@@ -66,7 +66,7 @@ status_code read_array(char *fname, film_array films, int *count_films)
 
 status_code read_film(FILE *f, film_struct *film)
 {
-    status_code result;
+    status_code result = ok;
     if (read_str(f, film->title, MAX_TITLE_LENGTH) \
         || read_str(f, film->surname, MAX_SURNAME_LENGTH) || (fscanf(f, "%d", &film->year) != 1))
         result = file_input_error;
