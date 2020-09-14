456d076da0cbf1b42cff36f9864f9a59f589e5af
Muravev Ilia <degtyarev327@gmail.com>
2020-09-14 19:51:42 +0300

    fix solution for lab_06

--- lab_06_01_01/main.c
+++ lab_06_01_01/main.c
@@ -3,12 +3,12 @@
 int main(int argc, char **argv)
 {
     status_code result = ok;
-    result = input_check(argc, argv);
+    // result = input_check(argc, argv);
     if (result == ok)
     {
         film_array films;
         int count_films = 0;
-        // result = read_array(argv[1], films, &count_films);
+        result = read_array(argv[1], films, &count_films);
         // if (result == ok)
         // {
         //     sort_array(films, count_films, argv[2]);
