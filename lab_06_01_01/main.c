7c26fb45b1ef3ab018104a11c5f128bdac2eb7ec
Muravev Ilia <degtyarev327@gmail.com>
2020-09-14 19:46:57 +0300

    fix solution for lab_06

--- lab_06_01_01/main.c
+++ lab_06_01_01/main.c
@@ -9,14 +9,14 @@ int main(int argc, char **argv)
         film_array films;
         int count_films = 0;
         result = read_array(argv[1], films, &count_films);
-        if (result == ok)
-        {
-            sort_array(films, count_films, argv[2]);
-            if (argc == 3)
-                print_array(films, count_films);
-            // else
-            //     binary_search(films, count_films, argv[2], argv[3]);
-        }
+        // if (result == ok)
+        // {
+        //     sort_array(films, count_films, argv[2]);
+        //     if (argc == 3)
+        //         print_array(films, count_films);
+        //     else
+        //         binary_search(films, count_films, argv[2], argv[3]);
+        // }
     }
     return result;
 }
