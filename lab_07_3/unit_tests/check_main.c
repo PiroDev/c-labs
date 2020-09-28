bbf9bbbc77078053e2787cc389d703002a4269e7
Muravev Ilia <degtyarev327@gmail.com>
2020-09-28 17:15:13 +0300

    add unit tests for lab_07

---
+++ lab_07_3/unit_tests/check_main.c
@@ -0,0 +1,42 @@
+#include "test_suites.h"
+#include <stdlib.h>
+
+int main(void)
+{
+    int tests_passed = 0;
+    Suite *equal_class;
+    SRunner *runner;
+
+    equal_class = is_even_suite(); /* is_even */
+    runner = srunner_create(equal_class);
+    srunner_run_all(runner, CK_VERBOSE);
+    tests_passed = srunner_ntests_failed(runner);
+
+    equal_class = swap_suite(); /* swap */
+    runner = srunner_create(equal_class);
+    srunner_run_all(runner, CK_VERBOSE);
+    tests_passed = srunner_ntests_failed(runner);
+
+    equal_class = cmp_int_suite(); /* cmp_int */
+    runner = srunner_create(equal_class);
+    srunner_run_all(runner, CK_VERBOSE);
+    tests_passed = srunner_ntests_failed(runner);
+
+    equal_class = copy_array_suite(); /* copy_array */
+    runner = srunner_create(equal_class);
+    srunner_run_all(runner, CK_VERBOSE);
+    tests_passed = srunner_ntests_failed(runner);
+
+    equal_class = key_suite(); /* key */
+    runner = srunner_create(equal_class);
+    srunner_run_all(runner, CK_VERBOSE);
+    tests_passed = srunner_ntests_failed(runner);
+
+    equal_class = mysort_suite(); /* my_sort */
+    runner = srunner_create(equal_class);
+    srunner_run_all(runner, CK_VERBOSE);
+    tests_passed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+
+    return (!tests_passed) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
