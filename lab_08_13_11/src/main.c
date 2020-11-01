2ef12c4d85618fa3aa841d6fd5d7f065e55c6869
Muravev Ilia <degtyarev327@gmail.com>
2020-11-01 14:01:21 +0300

    fix solution for lab_08

--- lab_08_13_11/src/main.c
+++ lab_08_13_11/src/main.c
@@ -48,37 +48,37 @@ int main(int argc, char **argv)
         }
 
         matrix_double_t * matrix_result = 0;
-        // if (!result)
-        // {
-        //     double det = 0.0;
-        //     switch (operation)
-        //     {
-        //         case 'a':
-        //             result = matrix_double_operation_add(matrix_1, matrix_2, matrix_1);
-        //             matrix_result = matrix_1;
-        //             break;
-        //         case 'm':
-        //             matrix_result = alloc_matrix_double(matrix_1->count_rows, matrix_2->count_columns);
-        //             if (matrix_result)
-        //                 result = matrix_double_operation_mult(matrix_1, matrix_2, matrix_result);
-        //             else
-        //                 result = error_out_of_memory;
-        //             break;
-        //         case 'o':
-        //             det = matrix_double_get_det(matrix_1, &result);
-        //             matrix_result = matrix_1;
-        //             break;
-        //         default:
-        //             break;
-        //     }
-        //     if (!result)
-        //     {
-        //         if (operation != 'o')
-        //             result = write_matrix_double_to_file(matrix_result, fname_output);
-        //         else
-        //             result = write_double_to_file(det, fname_output);
-        //     }
-        // }
+        if (!result)
+        {
+            double det = 0.0;
+            switch (operation)
+            {
+                case 'a':
+                    result = matrix_double_operation_add(matrix_1, matrix_2, matrix_1);
+                    matrix_result = matrix_1;
+                    break;
+                // case 'm':
+                //     matrix_result = alloc_matrix_double(matrix_1->count_rows, matrix_2->count_columns);
+                //     if (matrix_result)
+                //         result = matrix_double_operation_mult(matrix_1, matrix_2, matrix_result);
+                //     else
+                //         result = error_out_of_memory;
+                //     break;
+                // case 'o':
+                //     det = matrix_double_get_det(matrix_1, &result);
+                //     matrix_result = matrix_1;
+                //     break;
+                default:
+                    break;
+            }
+            // if (!result)
+            // {
+            //     if (operation != 'o')
+            //         result = write_matrix_double_to_file(matrix_result, fname_output);
+            //     else
+            //         result = write_double_to_file(det, fname_output);
+            // }
+        }
         free_matrix_double(&matrix_result);
         if (operation == 'm')
             free_matrix_double(&matrix_1);
