11d0a28bfb3319516d30b6ed114561aecf68131a
Muravev Ilia <degtyarev327@gmail.com>
2020-11-01 23:04:17 +0300

    add solution for lab_09

---
+++ lab_09_01_01/include/status_codes.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_STATUS_CODES_H_
+#define INCLUDE_STATUS_CODES_H_
+
+typedef enum {
+    ok = 0,
+    wrong_arguments_count = 1,
+    wrong_arguments_value = 2,
+    file_input_error = 3,
+    empty_file_error = 4,
+    wrong_year_format = 5,
+    out_of_memory = 6
+} status_code;
+
+#endif  // INCLUDE_STATUS_CODES_H_
